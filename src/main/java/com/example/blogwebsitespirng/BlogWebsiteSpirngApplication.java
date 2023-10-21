a25f3f79d6a15fe56fc4a9002f308a15c4ea4a87
VNKaiser <tandatvok16@gmail.com>
2023-10-21 09:53:26 +0700

    initial

---
+++ src/main/java/com/example/blogwebsitespirng/BlogWebsiteSpirngApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogwebsitespirng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWebsiteSpirngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogWebsiteSpirngApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blogwebsitespirng/BlogWebsiteSpirngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogwebsitespirng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWebsiteSpirngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
